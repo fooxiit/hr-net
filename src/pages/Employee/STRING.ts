28cb23479d399054ba1f9b2415ca71db282eff83
guillian <guillian.edo@gmail.com>
2026-06-27 06:48:16 +0200

    Correction de l'orthographe de "Start Datte" en "Start Date" dans STRING.ts

--- src/pages/Employee/STRING.ts
+++ src/pages/Employee/STRING.ts
@@ -2,7 +2,7 @@ const STRING = {
     TAB_LABEL: {
         FIRST_NAME: 'First Name',
         Last_Name: 'Last Name',
-        START_DATE: 'Start Datte',
+        START_DATE: 'Start Date',
         DEPARTMENT: 'Department',
         BIRTH_DATE: 'Date of Birth',
         STREET: 'Street',
